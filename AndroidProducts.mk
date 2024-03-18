065ce3609eddf411346a65914fcc913e11913e38
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-18 18:09:58 +0530

    a5y17lte: Redo makefiles

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,21 +1,13 @@
 #
-# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+# Copyright (C) 2022 Team Win Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a5y17lte.mk
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_a5y17lte.mk
-
-COMMON_LUNCH_CHOICES := twrp_a5y17lte-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_a5y17lte-user \
+    twrp_a5y17lte-userdebug \
+    twrp_a5y17lte-eng
--- twrp_a5y17lte.mk
+++ twrp_a5y17lte.mk
@@ -1,28 +1,30 @@
 #
-# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+# Copyright (C) 2022 Team Win Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from common AOSP config
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
-# Inherit some common TWRP stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-PRODUCT_DEVICE := a5y17lte
-PRODUCT_NAME := twrp_a5y17lte
+PRODUCT_RELEASE_NAME := a5y17lte
+
+# Inherit from a5y17lte device
+$(call inherit-product, device/samsung/a5y17lte/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
+PRODUCT_NAME := twrp_$(PRODUCT_RELEASE_NAME)
+BOARD_VENDOR := Samsung
+PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A5(2017)
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := Samsung
+TARGET_VENDOR := Samsung
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
